fc875a912b6d9df1bf6230003fccdb5e47853df0
Lluppiter <107683409+Lluppiter@users.noreply.github.com>
2022-09-07 10:48:38 +0000

    Deploying to gh-pages from @ Lluppiter/goit-js-hw-09@714e0892ab0ff87dbd04d6c070c94c2b1d8d603d 🚀

--- 01-color-switcher.e54baf3a.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");t.addEventListener("click",(function(e){backgroundColorGetRandomHexColor=setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(backgroundColorGetRandomHexColor),t.removeAttribute("disabled","")}))}();
-//# sourceMappingURL=01-color-switcher.e54baf3a.js.map
